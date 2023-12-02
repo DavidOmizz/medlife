76314edc21da3cdf89c6e91a9d7020bbd4c6db87
David <davidomisakin4good@gmail.com>
2023-12-02 21:26:00 +0100

    Added: Added a static root to my setting

--- medilife/settings.py
+++ medilife/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = '/home/medilife/medilife/static'
 
 from dotenv import load_dotenv
 import os
